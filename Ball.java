9f8531f2e7be398b999e4dd0be8a9ce402c40e79
Tiago-Lz <85238226+Tiago-Lz@users.noreply.github.com>
2022-04-01 12:51:01 -0300

    Update Ball.java

--- Ball.java
+++ Ball.java
@@ -14,7 +14,7 @@ public class Ball {
 		this.y = y;
 		this.width = 4; 
 		this.height = 4;
-		speed = 1.7;
+		speed = 2.0;
 		angle = new Random().nextInt(120-45)+ 46;
 		dx = Math.cos(Math.toRadians(angle));
 		dy = Math.sin(Math.toRadians(angle));
